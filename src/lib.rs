a86424f16f4dd6081d49c72fdb28203a02084adf
guillaume <lepro.guillaume@gmail.com>
2018-04-28 21:38:07 +0200

    Documentation, type changes, changed Envelope trait, removed useless test

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,68 @@
+//! This is a crate about Music Sequencing, give it a sequence and instruments to play with and it will output PCM.
+//! 
+//! In this library, everything related to time is in seconds and notes is in hertz, so please do any conversions beforehand.
+//! 
+//! Samples are all processed as double-precision floats.
+
 extern crate pcm;
 
 use pcm::{PCMParameters, PCM};
 use std::collections::HashMap;
 
+/// The sequencer itself
 pub struct MusicSequencer {
     pub pcm_parameters: PCMParameters,
     pub sequence: Sequence,
     pub instruments: InstrumentList,
 }
 
+/// Contains notes to play in a sequence
 pub struct Sequence {
     pub notes: Vec<Note>,
 }
 
+/// Information about a note in a sequence
 pub struct Note {
-    pub start_at: u32,
-    pub end_at: u32,
-    pub duration: u32,
+    pub start_at: f64,
+    pub end_at: f64,
+    pub duration: f64,
     pub frequency: f64,
-    pub on_velocity: u32,
-    pub off_velocity: u32,
+    pub on_velocity: f64,
+    pub off_velocity: f64,
     pub instrument_id: u16,
 }
 
+/// List of instruments used by the sequencer
 pub struct InstrumentList {
     pub instruments: HashMap<u16, Instrument>,
 }
 
+/// Defines how a note being played should sound
 pub struct Instrument {
     pub keys: HashMap<f64, Key>,
-    pub key_gen: Box<KeyGenerator>,
+    pub key_gen: Option<Box<KeyGenerator>>,
     pub loopable: bool,
     pub envelope: Option<Box<Envelope>>,
 }
 
+/// Sound for a particular frequency made by an instrument
 pub struct Key {
     pub audio: PCM,
 }
 
+/// Used for generating a new key for a particular frequency
 pub trait KeyGenerator {
-    fn key_gen(&self, frequency: f64) -> PCM;
+    /// Takes a frequency in Hertz and outputs PCM with specified parameters for the new key.
+    fn key_gen(&self, frequency: f64, parameters: PCMParameters) -> PCM;
 }
 
+/// Defines how the loudness for an instrument behaves with time
 pub trait Envelope {
-    fn before_sustain(&self, time: f64) -> f64;
-    fn sustain(&self) -> f64;
-    fn after_sustain(&self) -> f64;
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    /// Defines behavior from start to sustain included.
+    /// Time is in seconds and output is between 0 and 1.
+    fn before_during_sustain(&self, time: f64) -> f64;
+    /// Defines behavior after sustain.
+    /// Time starts when key is released and is in seconds.
+    /// Output (representing amplitude) should be between 0 and 1.
+    fn after_sustain(&self, time: f64) -> f64;
 }
